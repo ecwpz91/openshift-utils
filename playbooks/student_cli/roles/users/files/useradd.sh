be2e7edd8f1bee2de4f1be5670f18da9c81aebc2
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-10-22 22:38:28 -0400

    Added support for one-liner command with '\ &&'

--- playbooks/student_cli/roles/users/files/useradd.sh
+++ playbooks/student_cli/roles/users/files/useradd.sh
@@ -2,5 +2,5 @@
 
 for (( i = 1; i <= 30; i++ )); do /usr/bin/htpasswd -b /etc/origin/master/htpasswd "student$i" "student$i"; done
 
-groupadd oseuser
-for (( i = 1; i <= 30; i++ )); do useradd "student$i"; echo "student$i" | passwd "student$i" --stdin; usermod -G oseuser student$i; done
+groupadd oseuser \
+&& for (( i = 1; i <= 30; i++ )); do useradd "student$i"; echo "student$i" | passwd "student$i" --stdin; usermod -G oseuser student$i; done
